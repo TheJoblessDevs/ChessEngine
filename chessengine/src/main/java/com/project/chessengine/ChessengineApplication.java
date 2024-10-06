7dcddd932d45cd5f0208252e1d5cb9c274b4ece1
Jus-Jar <jaredheeralal04@gmail.com>
2024-10-06 14:42:02 -0400

    Springboot Project File

---
+++ chessengine/src/main/java/com/project/chessengine/ChessengineApplication.java
@@ -0,0 +1,13 @@
+package com.project.chessengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessengineApplication.class, args);
+	}
+
+}
